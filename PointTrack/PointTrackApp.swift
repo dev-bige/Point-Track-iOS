7578ef76dc3ff0d1a5db6f344e485584b185a85b
dev-bige <ethan.d.evans9@gmail.com>
2022-03-13 11:10:21 -0500

    Initial Commit

---
+++ PointTrack/PointTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointTrackApp.swift
+//  PointTrack
+//
+//  Created by Ethan Evans on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PointTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
